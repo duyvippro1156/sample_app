9a4bf996a4cdd474add672c310a228ca1808bdfe
duyvippro1156 <maiduybigbang@gmail.com>
2021-09-15 18:37:26 +0700

    done chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,55 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :load_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :current_user_admin, only: :destroy
+
+  def index
+    @users = User.page(params[:page]).per(Settings.show_limit.show_5)
+  end
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    return if @user
+    # @user = User.find_by id: params[:id]
+    # return if @user
 
-    flash[:warning] = t ".user_not_found"
-    redirect_to new_user_path
+    # flash[:warning] = t "users.user_not_found"
+    # redirect_to new_user_path
   end
+
+  def edit; end
 
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".welcome_home"
-      redirect_to @user
+      # @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "users.check_email_activated"
+      redirect_to login_path
     else
       render :new
     end
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "users.profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = "users.deleted"
+    else
+      flash[:danger] = "users.delete_fail"
+    end
+    redirect_to @user
   end
 
   private
@@ -27,5 +57,29 @@ class UsersController < ApplicationController
   def user_params
     params
       .require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user? @user
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "users.user_not_found"
+    redirect_to new_user_path
+  end
+
+  def current_user_admin
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, :email, :password, :password_confirmation, presence: true
-  validates :name, :password,
-            length: {minimum: Settings.validate.length.min_6}
-  validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX},
-                    length: {maximum: Settings.validate.length.max_45}
+  validates :name, presence: true,
+    length: {maximum: Settings.validate.length.max_255}
+  validates :password, presence: true,
+    length: {minimum: Settings.validate.length.min_6},
+    allow_nil: true
+  validates :email, format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true,
+    length: {maximum: Settings.validate.length.max_255},
+    presence: true
   has_secure_password
 
   # Returns the hash digest of the given string.
@@ -29,11 +38,33 @@ class User < ApplicationRecord
     update_column :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update_column :remember_digest, nil
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end

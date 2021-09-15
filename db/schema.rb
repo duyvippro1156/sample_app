f306e14e802b07d86488073e242e5a1dc5de06d2
duyvippro1156 <maiduybigbang@gmail.com>
2021-09-15 18:37:26 +0700

    done chapter 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_140823) do
+ActiveRecord::Schema.define(version: 2021_09_19_142222) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+
+    t.string "password_digest"
   end
 
 end
